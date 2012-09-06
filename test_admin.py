407f3612bed26426c0003e7276d7a64de14921dd
Chris Schwarz <cschwarz@thoughtworks.com>
2012-09-06 11:04:56 -0500

    Chris/Apurv - Added some functional tests for login

--- test_admin.py
+++ test_admin.py
@@ -15,25 +15,34 @@ class AdminTestCase(TestCase):
 		dbinit.seed()
 
 	def test_invalid_login(self):
-		resp = self.login('fakeuser', 'fakepass')
+		resp = self.login('fake', 'fake')
 		assert '<p class="error"><strong>Error:</strong> Invalid' in resp.data
 
-	def test_view_all_users_with_java(self):
-		resp = self.login('admin', 'admin')
-		assert '<title>Hacksaw - Admin</title>' in resp.data
+	def test_blank_login_fails(self):
+		resp = self.login()
+		assert '<p class="error"><strong>Error:</strong> Can' in resp.data
+
+	def test_admin_console(self):
+		resp = self.login('admin', 'admin', 'Admin')
+		self._view_all_users_with_skill_java(resp)
+		self._view_single_user_skills()
+
+	def _view_all_users_with_skill_java(self, resp):
 		resp = self.client.get('/admin/viewskill/Java')
 		assert 'Test User' in resp.data
 
-	def test_view_single_user(self):
-		self.login('admin', 'admin')
+	def _view_single_user_skills(self):
 		resp = self.client.get('/admin/viewuser/test')
 		assert 'Java' in resp.data
 
-	def login(self, username, password):
-		return self.client.post('/', data=dict(
+	def login(self, username='', password='', title=None):
+		resp = self.client.post('/', data=dict(
 			username=username,
 			password=password
 		), follow_redirects=True)
+		if title:
+			assert '<title>Hacksaw - '+title+'</title>' in resp.data
+		return resp
 
 if __name__ == '__main__':
 	unittest.main()
